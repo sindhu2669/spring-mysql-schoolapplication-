ffe0d6a5754850c78ba9fa36541a8e6f42beac20
sindhu senthil <sindhusenthil07@gmail.com>
2024-03-11 19:06:49 +0530

    one file added

---
+++ src/main/java/com/eazybytes/eazyschool/model/Holiday.java
@@ -0,0 +1,29 @@
+package com.eazybytes.eazyschool.model;
+
+import lombok.Data;
+
+@Data
+public class Holiday extends BaseEntity {
+
+    private String day;
+    private String reason;
+    private Type type;
+
+    public enum Type {
+        FESTIVAL, FEDERAL
+    }
+    public Holiday(String day, String reason, Type type){
+        this.day = day;
+        this.reason = reason;
+        this.type = type;
+    }
+    public  String getDay(){
+        return day;
+    }
+    public  String getReason(){
+        return reason;
+    }
+    public  Type getType(){
+        return  type;
+    }
+}
